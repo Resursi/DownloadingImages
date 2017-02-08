cb6dffa75090941fc179291ffadd8d70bec90356
Quaca <amar.kvakic@gmail.com>
2017-02-08 01:28:21 +0100

    gotovo

--- app/src/main/java/inworkster/downloadingimages/MainActivity.java
+++ app/src/main/java/inworkster/downloadingimages/MainActivity.java
@@ -1,15 +1,43 @@
 package inworkster.downloadingimages;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView downloadedImage;
+
+    //http://vignette4.wikia.nocookie.net/pokemon/images/5/5f/025Pikachu_OS_anime_11.png/revision/latest?cb=20150717063951
 
     public void downloadImage(View view){
 
-        
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myImage;
+
+        try {
+            myImage = task.execute("http://vignette4.wikia.nocookie.net/pokemon/images/5/5f/025Pikachu_OS_anime_11.png/revision/latest?cb=20150717063951").get();
+            downloadedImage.setImageBitmap(myImage);
+
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        } catch (ExecutionException e) {
+
+            e.printStackTrace();
+
+        }
 
     }
 
@@ -17,5 +45,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        downloadedImage = (ImageView)findViewById(R.id.imageView);
+
+    }
+
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
+
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+            return null;
+        }
     }
 }
